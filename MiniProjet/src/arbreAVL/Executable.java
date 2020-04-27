29dc06849bd6af05d3e0b17758733d34161cc494
Zedyon <64407060+Zedyon@users.noreply.github.com>
2020-04-27 12:18:26 +0200

    Add files via upload

---
+++ MiniProjet/src/arbreAVL/Executable.java
@@ -0,0 +1,17 @@
+package allibe_TPArbreAVL;
+
+import java.io.IOException;
+
+/**
+ * Voici la classe a executer pour tester le programme.
+ * @author allibmat (Mathis ALLIBE)
+ *
+ */
+public class Executable {
+
+	public static void main(String[] args) throws IOException {
+		Matrice matrice = new Matrice("TSP005");
+		matrice.affiche();
+	}
+
+}
