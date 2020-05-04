bf18e606876702a8346d7fc71d6a92030e3229bd
allibmat <allibmat@GI-12-ECNGP-RUG.AIPDS.aip>
2020-05-04 11:33:50 +0200

    test MAJ

--- MiniProjet/src/matrice/Executable.java
+++ MiniProjet/src/matrice/Executable.java
@@ -11,6 +11,8 @@ public class Executable {
 		System.out.println("test");
 		Matrice matrice = new Matrice("TSP005");
 		matrice.afficheMatrice();
+		matrice.reduxtion();
+		matrice.afficheMatrice();
 		Matrice matrice_red = matrice.retire(2, 2);
 		matrice_red.afficheMatrice();
 	}
